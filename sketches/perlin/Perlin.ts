61ca6a4ffc718ace0c6e0cd25b8f47628108644c
Ismail Magomedov <caspian.seagull@gmail.com>
2022-01-09 18:32:01 +0300

    Simplex noise update

--- sketches/perlin/Perlin.ts
+++ sketches/perlin/Perlin.ts
@@ -10,7 +10,7 @@ class Perlin extends P5Sketch<SketchParams> {
   render = (p: p5) => {
     this.p = p;
 
-    const Noise4D = makeNoise4D(567567);
+    const Noise4D = makeNoise4D(909);
 
     p.setup = () => {
       p.createCanvas(this.size, this.size);
@@ -26,12 +26,13 @@ class Perlin extends P5Sketch<SketchParams> {
 
       for (let j = 0; j < lines; j++) {
         p.beginShape();
-        for (let i = 0; i <= this.size; i += 10) {
-          let x = i,
-            y = 0;
+        for (let i = 0; i <= this.size; i += 8) {
+          let x = i;
+          let y = 0;
+
           p.fill(255);
 
-          let radius = 1;
+          let noiseRadius = this.params.radius;
           let distance = center - Math.abs(center - x);
           let intense = p.constrain(distance, 0, 100) / 100;
           let noiseAngle = (p.TWO_PI * (this.dt + j * 0.1)) / 10;
@@ -39,17 +40,17 @@ class Perlin extends P5Sketch<SketchParams> {
           let noiseX = Noise4D(
             this.scale * x + 100,
             this.scale * y + 100,
-            radius * Math.cos(noiseAngle),
-            radius * Math.sin(noiseAngle)
+            noiseRadius * Math.cos(noiseAngle),
+            noiseRadius * Math.sin(noiseAngle)
           );
           let noiseY = Noise4D(
             this.scale * x,
             this.scale * y,
-            radius * Math.cos(noiseAngle),
-            radius * Math.sin(noiseAngle)
+            noiseRadius * Math.cos(noiseAngle),
+            noiseRadius * Math.sin(noiseAngle)
           );
 
-          p.circle(x + intense * 50 * noiseX, y + 50 * intense * noiseY + j * 15, 2 + 5 * intense * (noiseX - noiseY));
+          p.circle(x + intense * 50 * noiseX, y + 50 * intense * noiseY + j * 15, 8 * intense * noiseY);
         }
         p.endShape();
       }
--- sketches/perlin/params.ts
+++ sketches/perlin/params.ts
@@ -1,15 +1,15 @@
-import { SketchParams } from './types'
+import { SketchParams } from './types';
 
 export const defaultParams: SketchParams = {
-  radius: 50
+  radius: 1
 };
 
 export const controls = {
   radius: {
     value: defaultParams.radius,
-    min: 10,
-    max: 100,
-    step: 1,
-    label: 'Radius'
+    min: 0.5,
+    max: 10,
+    step: 0.1,
+    label: 'Noise intense'
   }
-}
+};
